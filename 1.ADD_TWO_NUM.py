92c7b987fe10397afc0e0a959d031ff59a39c0bf
Sarvesh_96 <jardesarvesh12@gmail.com>
2026-01-21 10:50:36 +0530

    Add files via upload

---
+++ 1.ADD_TWO_NUM.py
@@ -0,0 +1,14 @@
+# pre define value to variable 
+a = 23
+b = 45
+c= a+b
+print("Addition of a + b is : ",c)
+
+#  WAP using taking user input
+
+c = float(input("enter a 1st number: "))
+d = float(input("enter a second number: "))
+
+e = c+d
+
+print("Addition of c+d is :", e)
