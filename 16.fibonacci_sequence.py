be3afd4bb9014500ee94ed3cb6d2753afb98f2b9
Sarvesh_96 <jardesarvesh12@gmail.com>
2026-01-23 16:19:32 +0530

    Add files via upload

---
+++ 16.fibonacci_sequence.py
@@ -0,0 +1,17 @@
+a = 0 
+b = 1
+
+num = int(input("enter a number: "))
+
+if num == 1:
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(1,num+1):
+        c = a+b
+        a = b
+        b = c
+        print(c)
+
+        
