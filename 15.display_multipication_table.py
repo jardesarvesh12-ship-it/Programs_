9166815abb7d2da3ddb59438e140dc913ca79d13
Sarvesh_96 <jardesarvesh12@gmail.com>
2026-01-23 16:19:13 +0530

    Add files via upload

---
+++ 15.display_multipication_table.py
@@ -0,0 +1,14 @@
+num = int(input("enter a number: "))
+
+for i in range(1,11):
+    print(num ," x ", i ,"=",num * i)
+
+
+
+#  Using While loop
+
+n = 7
+i = 1 
+while i <= 10:
+    print(n ," x ", i ,"=",n * i)
+    i += 1
